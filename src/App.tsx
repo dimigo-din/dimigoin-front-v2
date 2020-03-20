e2f4727699f1d3c15a2593ee84a2bbd3c8da4590
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2020-03-21 03:41:44 +0900

    feat(pages/main): layout for Main

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import styled from '@emotion/styled';
 
 import GlobalStyle from './components/GlobalStyle';
+import Footer from './components/Footer';
+
 import Router from './router';
 
 function App() {
@@ -9,7 +11,10 @@ function App() {
     <>
       <GlobalStyle />
       <Container>
-        <Router />
+        <RouterWrap>
+          <Router />
+        </RouterWrap>
+        <Footer />
       </Container>
     </>
   );
@@ -18,4 +23,12 @@ function App() {
 export default App;
 
 const Container = styled.div`
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+`;
+
+const RouterWrap = styled.main`
+  flex: 1 0 auto;
+  padding-bottom: 32px;
 `;
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,8 +1,160 @@
 import React from 'react';
 import styled from '@emotion/styled';
 
+import variables from '../scss/_variables.scss';
+
+import NaiveContainer from '../components/grids/NaiveContainer';
+import DimiCard from '../components/dimiru/DimiCard';
+import DimiIcon from '../components/dimiru/DimiIcon';
+
 const MainPage = () => {
-  return (null);
+  return (
+    <Container>
+      <Brand>
+        <BrandLogo />
+      </Brand>
+      <InfoContainer>
+        <Column>
+          <ProfileSection>
+            <ProfileCard>
+              <ProfileInfoLeft>
+                <ProfilePhoto />
+                <ProfileDefaultPhoto />
+                <ProfileInfo>
+                  <ProfileInfoSerial>
+                    2학년 5반
+                  </ProfileInfoSerial>
+                  <ProfileInfoName>
+                    여준호
+                  </ProfileInfoName>
+                </ProfileInfo>
+              </ProfileInfoLeft>
+
+              <ButtonList>
+                <Button
+                  icon="setting"
+                  title="설정"
+                  pointer
+                />
+                <Button
+                  icon="logout"
+                  title="로그아웃"
+                  pointer
+                />
+              </ButtonList>
+            </ProfileCard>
+          </ProfileSection>
+          <InfoSection>
+            <InfoCard>
+              <InfoNotice>
+                학년별 밴드에 교과별 온라인 학습이 공지되었습니다. 반드시 확인하세요.
+              </InfoNotice>
+            </InfoCard>
+          </InfoSection>
+        </Column>
+        <Column>
+          <MealSection>
+          </MealSection>
+        </Column>
+      </InfoContainer>
+    </Container>
+  );
 };
 
 export default MainPage;
+
+const Container = styled(NaiveContainer)`
+  padding-top: 1rem;
+`;
+
+const Brand = styled.h1`
+  margin: 2rem 0 1rem 0.5rem;
+  font-size: 36px;
+  font-weight: ${variables.fontWeightExtraBold};
+`;
+
+const BrandLogo = styled.img`
+  width: 150px;
+`;
+
+const InfoContainer = styled.div`
+`;
+
+const Column = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  align-items: stretch;
+  margin: 0.5rem;
+`;
+
+const ProfileSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 1rem;
+`;
+
+const ProfileCard = styled(DimiCard)`
+  display: flex;
+  justify-content: space-between;
+  flex: 1;
+  min-height: 46px;
+  align-items: center;
+`;
+
+const ProfileInfoLeft = styled.div`
+  display: flex;
+  flex: 1;
+  align-items: center;
+`;
+
+const ProfilePhoto = styled.img`
+  border: 1px solid ${variables.grayLighter};
+  border-radius: 50%;
+  object-fit: cover;
+  width: 44px;
+  height: 44px;
+  margin-right: 15px;
+`;
+
+const ProfileDefaultPhoto = styled.span`
+`;
+
+const ProfileInfo = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  font-size: 20px;
+`;
+
+const ProfileInfoName = styled.span`
+  color: ${variables.black};
+`;
+
+const ProfileInfoSerial = styled.span`
+  color: ${variables.gray};
+  margin-right: 5px;
+`;
+
+const ButtonList = styled.nav`
+`;
+
+const Button = styled(DimiIcon)`
+  cursor: pointer;
+  font-size: 23px;
+
+  &:not(:last-child) {
+    margin-right: 0.25em;
+  }
+`;
+
+const InfoSection = styled.section`
+`;
+
+const InfoCard = styled(DimiCard)`
+`;
+
+const InfoNotice = styled.p`
+`;
+
+const MealSection = styled.section`
+`;
