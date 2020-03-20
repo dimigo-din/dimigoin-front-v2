fe2c2d9dde06706508e69adae3d4ff0682a62f66
reactericdev <reactdev@kakao.com>
2020-03-20 12:18:30 +0900

    chore: install storybook

---
+++ .storybook/config.js
@@ -0,0 +1,10 @@
+import { configure } from "@storybook/react";
+import requestContext from "require-context.macro";
+
+const req = requestContext("../src/components", true, /\.stories\.js$/);
+
+function loadStories() {
+  req.keys().forEach(filename => req(filename));
+}
+
+configure(loadStories, module);
