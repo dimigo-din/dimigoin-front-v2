6942e98c9f937fee7af5902b7502cfcfb9ccdec8
정한 <35295182+reactericdev@users.noreply.github.com>
2020-03-23 08:57:14 +0900

    feat(components/dimiru): create DimiButton (#39)
    * feat(components/dimiru): createnDimiButton
    
    * fix : wrong button style

---
+++ src/components/dimiru/DimiButton.tsx
@@ -0,0 +1,97 @@
+import React from "react";
+import css from "@emotion/css";
+import variables from "../../scss/_variables.scss";
+
+type DimiButtonProps = {
+  gray?: boolean;
+  active?: boolean;
+  loading?: boolean;
+  href?: string;
+  small?: boolean;
+  large?: boolean;
+  text?: boolean;
+  click?: () => void;
+  children: React.ReactNode | string;
+};
+
+const DimiButton: React.FC<DimiButtonProps> = ({
+  gray = false,
+  active = true,
+  loading = false,
+  href = undefined,
+  small = false,
+  large = false,
+  text = false,
+  click,
+  children
+}) => {
+  const css = [
+    style.btn,
+    gray && style.gray,
+    text && style.text,
+    (loading || !active) && style.disbleCurser,
+    large && style.large,
+    small && style.small
+  ].filter(Boolean);
+  return (
+    <a
+      href={href}
+      css={css}
+      {...(active && {
+        onClick: click
+      })}
+    >
+      {children}
+    </a>
+  );
+};
+
+export default DimiButton;
+
+const style = {
+  btn: css`
+    position: relative;
+    display: inline-flex;
+    overflow: hidden;
+    align-items: center;
+    justify-content: center;
+    padding: 0.8em 2.7em;
+    appearance: none;
+    background-color: ${variables.red};
+    border-radius: 2rem;
+    box-shadow: 0 10px 24px 0 rgba(234, 51, 51, 0.61);
+    color: ${variables.white};
+    cursor: pointer;
+    font-weight: ${variables.fontWeightBold};
+    text-decoration: none;
+    text-shadow: 2px 2px 3px rgba(248, 105, 105, 0.9);
+    transition: all 0.2s ease-in-out, 0.5s background-color ease;
+    user-select: none;
+    white-space: nowrap;
+
+    &:hover {
+      box-shadow: 0 5px 12px 0 rgba(234, 51, 51, 0.41);
+    }
+  `,
+  gray: css`
+    background-color: ${variables.grayLighten};
+    box-shadow: 0 10px 24px 0 rgba(50, 50, 50, 0.11);
+    color: ${variables.grayDark};
+    text-shadow: 2px 2px 3px rgba(255, 255, 255, 0.35);
+    &:hover {
+      box-shadow: 0 5px 12px 0 rgba(234, 234, 234, 0.41);
+    }
+  `,
+  text: css`
+    background-color: transparent;
+  `,
+  disbleCurser: css`
+    cursor: not-allowed;
+  `,
+  large: css`
+    font-size: 24px;
+  `,
+  small: css`
+    font-size: 12px;
+  `
+};
