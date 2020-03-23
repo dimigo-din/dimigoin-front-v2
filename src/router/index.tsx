e898247ffef82f6036a789a2165a1e8422123ee3
reactericdev <reactdev@kakao.com>
2020-03-23 14:45:56 +0900

    feat: mainpage

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -3,14 +3,16 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import {
   Main, Draft, Meal, ExplorerFound, NotFound,
 } from '../pages';
+import Auth from './Auth';
 
 const Router: React.FC = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={Main} />
       <Route exact path="/draft" component={Draft} />
-      <Route exact path="/meal" component={Meal} />
+      <Route exact path="/melal" component={Meal} />
       <Route exact path="/ienope" component={ExplorerFound} />
+      <Route path="/auth" component={Auth} />
       <Route component={NotFound} />
     </Switch>
   </BrowserRouter>
