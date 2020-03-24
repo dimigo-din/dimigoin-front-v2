bd38b1674d60c58f6884f0131808c5498e13ece7
Lee Seungmin <lee@seungmin.io>
2020-03-25 01:15:31 +0900

    chore: edit login page (logic, style, ...)

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,12 +1,17 @@
-import * as React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import styled from '@emotion/styled';
 import css, { SerializedStyles } from '@emotion/css';
+
+import api from '../api';
+import auth from '../utils/auth';
 
 import DimiCard from '../components/dimiru/DimiCard';
 import DimiInput from '../components/dimiru/DimiInput';
 import DimiButton from '../components/dimiru/DimiButton';
 import DimiDivider from '../components/dimiru/DimiDivider';
+
+import SweetAlert from '../utils/swal';
 
 import variables from '../scss/_variables.scss';
 
@@ -14,18 +19,19 @@ type TStyleByDeviceWidth = {
   [key in 'tablet' | 'desktop']: SerializedStyles;
 };
 
-const until = (device: 'tablet' | 'desktop', style: string) => (({
-  tablet: css`
+const until = (device: 'tablet' | 'desktop', style: string) =>
+  (({
+    tablet: css`
       @media only screen and (max-width: 769px) {
         ${style}
       }
     `,
-  desktop: css`
+    desktop: css`
       @media only screen and (max-width: 769px) {
         ${style}
       }
     `,
-} as TStyleByDeviceWidth)[device]);
+  } as TStyleByDeviceWidth)[device]);
 
 const ContentMT = css`
   margin-top: 7em;
@@ -36,48 +42,100 @@ const LoginInput = css`
   margin-bottom: 1rem;
 `;
 
-export default () => (
-  <Container
-    css={css`
-      padding: 0 0.5rem;
-      margin-right: auto;
-      margin-left: auto;
-    `}
-  >
-    <CLogin>
-      <Section className="section">
-        <SectionTitle className="section__title">로그인</SectionTitle>
-        <Content css={ContentMT}>
-          <DimiInput
-            v-model="id"
-            css={LoginInput}
-            placeholder="아이디"
-            type="text"
-          />
-          <DimiInput
-            v-model="password"
-            css={LoginInput}
-            placeholder="비밀번호"
-            type="password"
-          />
-          <SubmitButton large>LOGIN</SubmitButton>
-          <RegisterDescription>
-            또는
-            <RegisterLink to="/">회원가입</RegisterLink>
-          </RegisterDescription>
-        </Content>
-      </Section>
-      <DimiDivider vertical />
-      <Section className="section">
-        <SectionTitle>
-          {true ? '내일의 급식' : '오늘의 급식'}
-          {/* 여기는 추후에 mealgroup api로 대체해주세요 */}
-        </SectionTitle>
-        <Content>{/* <MealGroup /> */}</Content>
-      </Section>
-    </CLogin>
-  </Container>
-);
+export default () => {
+  const history = useHistory();
+
+  const [info, setInfo] = useState({ username: '', password: '' });
+  const [active, setActive] = useState<boolean>(true);
+
+  useEffect(() => {
+    localStorage.clear();
+  }, []);
+
+  const handleLogin = async () => {
+    await setActive(false);
+    try {
+      const request = await api.post('/auth', { ...info });
+      await auth.setToken(request.data.accessToken);
+      const { data: userInfo } = await api.get('/user/me');
+      await auth.setUserInfo(userInfo.identity);
+      await history.push('/');
+    } catch ({ response }) {
+      await setActive(true);
+      await SweetAlert.error(response.data.message);
+    }
+  };
+
+  return (
+    <Container
+      css={css`
+        padding: 0 0.5rem;
+        margin-right: auto;
+        margin-left: auto;
+      `}
+    >
+      <CLogin>
+        <Section className="section">
+          <SectionTitle className="section__title">로그인</SectionTitle>
+          <Content css={ContentMT}>
+            <Form
+              onSubmit={(e) => {
+                e.preventDefault();
+                handleLogin();
+              }}
+            >
+              <DimiInput
+                css={LoginInput}
+                placeholder="아이디"
+                type="text"
+                value={info.username}
+                onChange={(e) => {
+                  e.persist();
+                  setInfo((prevState) => ({
+                    ...prevState,
+                    username: e.target.value,
+                  }));
+                }}
+              />
+              <DimiInput
+                css={LoginInput}
+                placeholder="비밀번호"
+                type="password"
+                value={info.password}
+                onChange={(e) => {
+                  e.persist();
+                  setInfo((prevState) => ({
+                    ...prevState,
+                    password: e.target.value,
+                  }));
+                }}
+              />
+              <DimiButton
+                css={SubmitButton}
+                active={active}
+                click={handleLogin}
+              >
+                LOGIN
+              </DimiButton>
+            </Form>
+            <RegisterDescription>
+              또는{'  '}
+              <RegisterLink to="/">회원가입</RegisterLink>
+            </RegisterDescription>
+          </Content>
+        </Section>
+        <DimiDivider vertical />
+        <Section className="section">
+          <SectionTitle>
+            {false ? '내일의 급식' : '오늘의 급식'}
+            {/* 여기는 추후에 mealgroup api로 대체해주세요 */}
+          </SectionTitle>
+          <Content>{/* <MealGroup /> */}</Content>
+        </Section>
+      </CLogin>
+    </Container>
+  );
+};
 
 const Container = styled.div`
   display: flex;
@@ -115,7 +173,7 @@ const CLogin = styled(DimiCard)`
     `display: block;
       width: unset;`,
   )}
-  .section:first-child {
+  .section:first-of-type {
     order: 2;
   }
   .section:last-child {
@@ -131,9 +189,16 @@ const Section = styled.div`
   ${until('tablet', 'width: unset')}
 `;
 
-const SubmitButton = styled(DimiButton)`
-  align-self: center;
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const SubmitButton = css`
+  font-size: 24px;
   padding: 0.625em 2.75em;
+  align-self: center;
   margin-top: 3rem;
   font-weight: ${variables.fontWeightExtraBold};
   ${until('tablet', 'margin-top: 0.8rem')}
