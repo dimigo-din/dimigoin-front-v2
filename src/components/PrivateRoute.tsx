27f2c0cdb865333220fd50321c1e6d9ddd096bb5
Lee Seungmin <lee@seungmin.io>
2020-03-24 23:27:56 +0900

    chore: fix item name

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -9,7 +9,7 @@ const PrivateRoute = ({ component: Component, ...rest }: IProps) => (
   <Route
     {...rest}
     render={props =>
-      localStorage.getItem('token') ? (
+      localStorage.getItem('accessToken') ? (
         <Component {...props} />
       ) : (
         <Redirect
