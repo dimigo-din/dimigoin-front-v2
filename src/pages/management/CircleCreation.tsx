083c76f702deb02368b686cf0394e5000e7ae251
JUNO <hanaro0704@naver.com>
2020-03-25 03:07:06 +0900

    feat: CircleCreate page (#84)
    * Merge develop into master  (#34)
    
    * chore: install emotion(CSS-in-JS) (#9)
    
    * refactor: styled-components to emotion
    
    * chore(storybook): setup storybook (#21)
    
    * chore: install storybook
    
    * chore: intergrate dimiru with storybook
    
    * refactor: change page import style (#23)
    
    * chore(lint): configure eslint with husky, lint staged (#22)
    
    Co-authored-by: JUNO <hanaro0704@naver.com>
    
    * chore(style): re-setting emotion(CSS-in-JS) (#24)
    
    * feat(pages): implement Main page (#26)
    
    * feat(pages/main): layout for Main
    
    * feat(pages/main): align columns and show logo, default photo
    
    * feat: IE redirect to static html file (#27)
    
    * [ImgBot] Optimize images (#28)
    
    /public/static/font/dimigoincon.svg -- 31.27kb -> 30.62kb (2.09%)
    
    Signed-off-by: ImgBotApp <ImgBotHelp@gmail.com>
    
    Co-authored-by: ImgBotApp <ImgBotHelp@gmail.com>
    
    * refactor(style): apply Eslint fixes (#30)
    
    * refactor(style): apply eslint fixes
    
    * fix(style): add newlines in stories
    
    * feat(components/dimiru): create DimiInput (#31)
    
    * feat(components/dimiru): create DimiInput
    
    * feat(components/dimiru): errorMessage
    
    * fix: correct DOM format
    
    * refactor(style): add line break
    
    * feat(ci/cd) : add github action function 
    
    * feat: setup default github action (main.yml)
    
    * chore(ci/cd): setup Slack Notification
    
    Co-authored-by: JUNO <hanaro0704@naver.com>
    Co-authored-by: JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
    
    * Feat(CI/CD) : Github action for auto deploy is working (i-25)
    
    * Create main.yml
    
    * Update main.yml
    
    * chore(ci/cd): setup Slack Notification
    
    * chore(ci/cd) : change target branch to "develop" to test
    
    * chore(ci/cd): remove needs and runs-on
    
    * fix : change action-slack package name
    
    * fix : remove unused action package
    
    * fix : remove "yarn start"
    
    * fix : change branch to master & set nginx deploy
    
    Co-authored-by: JUNO <hanaro0704@naver.com>
    Co-authored-by: JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
    
    Co-authored-by: Lee Seungmin <lee@seungmin.io>
    Co-authored-by: 정한 <35295182+reactericdev@users.noreply.github.com>
    Co-authored-by: 심재성 <35318955+Jaeseong-CS@users.noreply.github.com>
    Co-authored-by: JUNO <hanaro0704@naver.com>
    Co-authored-by: imgbot[bot] <31301654+imgbot[bot]@users.noreply.github.com>
    Co-authored-by: ImgBotApp <ImgBotHelp@gmail.com>
    Co-authored-by: JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
    
    * fix : add command "generate target folder"
    
    * fix : error in generate target folder
    
    * chore : rename slack channel name
    
    * Deploying to production (#82)
    
    * feat: add animation for application loading (#64)
    
    * feat: add animation for application loading
    
    * chore: apply prettier
    
    * fix: to small text (#75)
    
    * feat(dimiru/DimiButtonGroup) : add dimibuttongroup (#77)
    
    * feat : HTTP API Library support (#78)
    
    * feat(API) : Circle API Support (#79)
    
    * feat: PrivateRoute for before login (#80)
    
    * chore: apply PrivateRoute
    
    * chore: fix PrivateRoute
    
    * chore: fix item name
    
    * feat: user (#81)
    
    * chore: auth util file
    
    * chore: edit api header
    
    * fix: fix button for login page
    
    * chore: fix onChange type
    
    * chore: use auth util instead localstorage
    
    * chore: edit login page (logic, style, ...)
    
    * chore: use userinfo
    
    * chore: delete photoUrl
    
    * fix: circle application loading style
    
    * refactor: destructuring assignment
    
    * refactor: destructuring assignment
    
    * chore: for now deployment
    
    * chore: lint fix
    
    Co-authored-by: 정한 <35295182+reactericdev@users.noreply.github.com>
    
    * feat(pages/manage): add CircleCreation, CircleManage
    
    * feat(pages/manage): form for CircleCreation

---
+++ src/pages/management/CircleCreation.tsx
@@ -0,0 +1,101 @@
+/* eslint-disable no-nested-ternary */
+import * as React from 'react';
+import styled from '@emotion/styled';
+
+import ContentWrapper from '../../components/ContentWrapper';
+import Dimigoincon from '../../components/Dimigoincon';
+import DimiCard from '../../components/dimiru/DimiCard';
+import DimiLongInput from '../../components/dimiru/DimiLongInput';
+import DimiInput from '../../components/dimiru/DimiInput';
+import DimiButton from '../../components/dimiru/DimiButton';
+
+const CircleCreation: React.FC = () => (
+  <ContentWrapper
+    header={(
+      <h1>
+        <Dimigoincon icon="edit" />
+        동아리 개설 신청
+      </h1>
+      )}
+  >
+    <DimiCard>
+      <FormRow>
+        <FormName>
+          동아리명
+        </FormName>
+        <DimiInput
+          placeholder="동아리 이름을 입력하세요."
+        />
+      </FormRow>
+      <FormRow>
+        <FormField>
+          <FormName>
+            동아리장
+          </FormName>
+          <DimiNumberInput
+            placeholder="학번"
+            maxLength={4}
+          />
+        </FormField>
+        <FormField>
+          <FormName>
+            부동아리장
+          </FormName>
+          <DimiNumberInput
+            placeholder="학번"
+            maxLength={4}
+          />
+        </FormField>
+      </FormRow>
+      <FormRow>
+        <DimiLongInput
+          height={150}
+          placeholder="동아리 설명을 입력해 주세요."
+        />
+      </FormRow>
+      <FormRow>
+        <DimiInput
+          placeholder="유튜브 홍보영상 링크를 입력해 주세요."
+        />
+      </FormRow>
+      <ButtonWrap>
+        <DimiButton>
+          제출하기
+        </DimiButton>
+      </ButtonWrap>
+    </DimiCard>
+  </ContentWrapper>
+);
+
+export default CircleCreation;
+
+const FormRow = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 1rem;
+`;
+
+const FormField = styled.div`
+  display: flex;
+  align-items: center;
+
+  &:first-child {
+    margin-right: 2rem;
+  }
+`;
+
+const FormName = styled.span`
+  min-width: fit-content;
+  margin-right: 1rem;
+  color: #191616;
+  font-weight: 600;
+`;
+
+const DimiNumberInput = styled(DimiInput)`
+  width: 8rem;
+`;
+
+const ButtonWrap = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`;
--- src/router/Management.tsx
+++ src/router/Management.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import PrivateRoute from '../components/PrivateRoute';
 
-import { Circle } from '../pages/management';
+import { Circle, CircleCreation, CircleManage } from '../pages/management';
 import ManagementWrapper from '../pages/management/ManagementWrapper';
 
 export default ({ match }: { match: { path: string } }) => (
   <ManagementWrapper>
-    <PrivateRoute path={`${match.path}/circle`} component={Circle} />
+    <PrivateRoute exact path={`${match.path}/circle`} component={CircleCreation} />
+    <PrivateRoute path={`${match.path}/circle/application`} component={Circle} />
+    <PrivateRoute path={`${match.path}/circle/manage`} component={CircleManage} />
   </ManagementWrapper>
 );
