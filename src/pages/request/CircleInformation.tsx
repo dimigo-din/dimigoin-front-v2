049cada9ba5b7d5a4e640fc6f0aed64850e22e20
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2020-03-28 13:59:44 +0900

    refactor: remove unused import of Link

--- src/pages/request/CircleInformation.tsx
+++ src/pages/request/CircleInformation.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 import Modal from 'react-responsive-modal';
 import { filter } from 'lodash';
