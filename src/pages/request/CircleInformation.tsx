f49c98e5aa101a401e879c6ece6234a80f9ca1da
reactericdev <reactdev@kakao.com>
2020-04-05 00:06:10 +0900

    feat : add final add button to circlecard

--- src/pages/request/CircleInformation.tsx
+++ src/pages/request/CircleInformation.tsx
@@ -3,9 +3,11 @@ import { useHistory } from 'react-router-dom';
 import Modal from 'react-responsive-modal';
 import { filter } from 'lodash';
 import styled from '@emotion/styled';
-import { useQuery } from '@apollo/react-hooks';
+import { useQuery, useMutation } from '@apollo/react-hooks';
 import gql from 'graphql-tag';
 
+import Swal from 'sweetalert2';
+import SweetAlert from '../../utils/swal';
 import CircleCard from '../../components/CircleCard';
 import DimiLoading from '../../components/dimiru/DimiLoading';
 import DimiButton from '../../components/dimiru/DimiButton';
@@ -80,6 +82,14 @@ const LOAD_CIRCLE_INFO = gql`
   }
 `;
 
+const SET_FINAL_CIRCLE = gql`
+  mutation($circleId: ID!) {
+    setFinal(ciecle: $circleId) {
+      _id
+    }
+  }
+`;
+
 const CircleInformation = () => {
   const history = useHistory();
 
@@ -100,6 +110,7 @@ const CircleInformation = () => {
   const { data: circledata, refetch: circleDataRefetch } = useQuery(
     LOAD_CIRCLE_INFO,
   );
+  const [setFinalCircle] = useMutation(SET_FINAL_CIRCLE);
 
   useEffect(() => {
     if (circlesData) {
@@ -148,6 +159,19 @@ const CircleInformation = () => {
                 onClick={() => {
                   setSelectedCircle(application.circle._id);
                   setOpen(true);
+                }}
+                onFinalSelect={() => {
+                  SweetAlert.confirm(`정말로 ${application.circle.name}을 확정하시겠습니까? 되돌릴 수 없습니다.`).then((e) => {
+                    if (e.value) return SweetAlert.confirm(`신중하게 생각해주세요. 정말로 ${application.circle.name}을 확정하시겠습니까?`);
+                  }).then((e) => {
+                    if (e?.value) console.log('찐확정');
+                    else Swal.fire('', '신중하게 생각해보세요.', 'question');
+                  });
+                  // setFinalCircle({
+                  //   variables: {
+                  //     circleId: application.circle._id,
+                  //   },
+                  // });
                 }}
                 imageKey={application.circle.imageKey}
                 name={application.circle.name}
