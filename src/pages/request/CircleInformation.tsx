e826b4218441a3344b77bb1f627b138ecfbd023b
정한 <35295182+reactericdev@users.noreply.github.com>
2020-04-06 00:53:40 +0900

    feat : Add final select button (#174)
    * feat : add final add button to circlecard
    
    * feat : add final add button to circlecard
    
    * fix : remove local setting
    
    * feat : final selection is now available

--- src/pages/request/CircleInformation.tsx
+++ src/pages/request/CircleInformation.tsx
@@ -3,9 +3,11 @@ import { useHistory } from 'react-router-dom';
 import Modal from 'react-responsive-modal';
 import { filter } from 'lodash';
 import styled from '@emotion/styled';
-import { useQuery } from '@apollo/react-hooks';
+import { useQuery, useMutation } from '@apollo/react-hooks';
 import gql from 'graphql-tag';
 
+import Swal from 'sweetalert2';
+import SweetAlert from '../../utils/swal';
 import CircleCard from '../../components/CircleCard';
 import DimiLoading from '../../components/dimiru/DimiLoading';
 import DimiButton from '../../components/dimiru/DimiButton';
@@ -80,6 +82,16 @@ const LOAD_CIRCLE_INFO = gql`
   }
 `;
 
+const SET_FINAL_CIRCLE = gql`
+  mutation($circleId: ID!) {
+    setFinalCircle(input: {
+      circle: $circleId
+    }) {
+      _id
+    }
+  }
+`;
+
 const CircleInformation = () => {
   const history = useHistory();
 
@@ -96,10 +108,15 @@ const CircleInformation = () => {
   ] = useState<ICircle | null>();
 
   const { data: circlesData } = useQuery(LOAD_CIRCLES);
-  const { data: appliedCirclesData } = useQuery(LOAD_APPLIED_CIRCLES);
+  const { data: appliedCirclesData, refetch: reloadAppliedCirclesData } = useQuery(LOAD_APPLIED_CIRCLES);
   const { data: circledata, refetch: circleDataRefetch } = useQuery(
     LOAD_CIRCLE_INFO,
   );
+  const [setFinalCircle] = useMutation(SET_FINAL_CIRCLE, {
+    onCompleted() {
+      reloadAppliedCirclesData();
+    },
+  });
 
   useEffect(() => {
     if (circlesData) {
@@ -148,6 +165,19 @@ const CircleInformation = () => {
                 onClick={() => {
                   setSelectedCircle(application.circle._id);
                   setOpen(true);
+                }}
+                onFinalSelect={() => {
+                  SweetAlert.confirm(`정말로 ${application.circle.name}을 확정하시겠습니까? 되돌릴 수 없습니다.`).then((e) => {
+                    if (e.value) return SweetAlert.confirm(`신중하게 생각해주세요. 정말로 ${application.circle.name}을 확정하시겠습니까?`);
+                  }).then((e) => {
+                    if (e?.value) {
+                      setFinalCircle({
+                        variables: {
+                          circleId: application.circle._id,
+                        },
+                      });
+                    } else Swal.fire('', '신중하게 생각해보세요.', 'question');
+                  });
                 }}
                 imageKey={application.circle.imageKey}
                 name={application.circle.name}
