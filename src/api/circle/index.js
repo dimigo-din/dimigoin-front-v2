a9cfa530bf08665f6bdaafde60db72f2273d63f8
Lee Seungmin <lee@seungmin.io>
2020-04-14 19:00:42 +0900

    refactor(api/circle): remove files

--- src/api/circle/index.js
+++
@@ -1,4 +0,0 @@
-import { CircleRequestorService, CircleManagerService } from './circle.service';
-
-export const circleRequestor = new CircleRequestorService('/circle');
-export const circleManager = new CircleManagerService('/circle');
